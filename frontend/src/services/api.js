260509f4e71df1e170a28b2a37e1ba1988c1e945
A-Saxena27 <saxenanu05@gmail.com>
2026-05-25 17:36:19 +0530

    fix production api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://grievance-management-system-np4q.onrender.com/api",
 });
 
 export default API;
