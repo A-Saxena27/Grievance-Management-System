c7679b1dea17da35562be44b1eb84b09e88be4a0
A-Saxena27 <saxenanu05@gmail.com>
2026-05-25 18:50:49 +0530

    fix env and db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,10 +3,9 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-
     console.log("MongoDB Connected");
-  } catch (error) {
-    console.log(error);
+  } catch (err) {
+    console.error("DB Error:", err.message);
     process.exit(1);
   }
 };
